8699fe97cc236f63f58d6a5bf12d191b2c7e7fc5
wangbeibeibei <1714541556@qq.com>
2019-05-20 19:58:35 +0800

    develop第一次提交主页面搭建

---
+++ app/src/main/java/com/jiyun/wanandroid/ui/project/ProjectFragment.java
@@ -0,0 +1,29 @@
+package com.jiyun.wanandroid.ui.project;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProjectFragment extends Fragment {
+
+
+    public ProjectFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(com.jiyun.wanandroid.R.layout.fragment_project, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/jiyun/wanandroid/ui/wechat/The_publicFragment.java
@@ -0,0 +1,29 @@
+package com.jiyun.wanandroid.ui.wechat;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class The_publicFragment extends Fragment {
+
+
+    public The_publicFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(com.jiyun.wanandroid.R.layout.fragment_the_public, container, false);
+    }
+
+}
