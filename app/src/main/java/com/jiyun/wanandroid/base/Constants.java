faba321459e8a63d1a87bec8ac4a1982a952807b
Jesuslovestheworld <1367346501@qq.com>
2019-05-21 09:55:22 +0800

    1.高蒙恩  日夜间模式

--- app/src/main/java/com/jiyun/wanandroid/base/Constants.java
+++ app/src/main/java/com/jiyun/wanandroid/base/Constants.java
@@ -25,4 +25,14 @@ public interface Constants {
     String PATH_CACHE = PATH_DATA + "/NetCache";
 
 
+
+
+    String DATA = "data";
+    //夜间模式
+    String MODE = "mode";
+    String NIGHT_CURRENT_FRAG_POS = "fragment_pos";
+    //保存设置日夜间模式时碎片的position
+    String DAY_NIGHT_FRAGMENT_POS = "day_night_fragment_pos";
+
+
 }
