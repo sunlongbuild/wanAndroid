deb9802be1de9f8f1ebf302e502a08efcf425150
sunlongbuild <357155057@qq.com>
2019-05-21 16:51:04 +0800

    孙龙:todo页面整体布局

---
+++ app/src/main/java/com/jiyun/wanandroid/ui/todo/activity/AddToDoActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.wanandroid.ui.todo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jiyun.wanandroid.R;
+
+public class AddToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
+}
