f9d4524b170b26a6a79e2da0e5cefc17b0c53bd1
xiaomidic <487353653@qq.com>
2019-05-22 09:41:38 +0800

    李帅 develop第三次提交：导航 流式布局

--- app/src/main/java/com/jiyun/wanandroid/ui/navigation/fragment/NavigationFragment.java
+++ app/src/main/java/com/jiyun/wanandroid/ui/navigation/fragment/NavigationFragment.java
@@ -1,13 +1,16 @@
 package com.jiyun.wanandroid.ui.navigation.fragment;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,6 +18,10 @@ import com.jiyun.wanandroid.R;
 import com.jiyun.wanandroid.base.BaseFragment;
 import com.jiyun.wanandroid.entity.navigation.NavigationBean;
 import com.jiyun.wanandroid.presenter.navigation.NavigationP;
+import com.jiyun.wanandroid.ui.MainActivity;
+import com.jiyun.wanandroid.ui.navigation.activity.FlowWebActivity;
+import com.jiyun.wanandroid.ui.navigation.adapter.MyNavigationAdapter;
+import com.jiyun.wanandroid.utils.ToastUtil;
 import com.jiyun.wanandroid.view.navigation.NavigationV;
 
 import java.util.ArrayList;
@@ -41,6 +48,8 @@ public class NavigationFragment extends BaseFragment<NavigationV, NavigationP> i
     private ArrayList<NavigationBean.DataBean> list;
     private View view;
     private LinearLayoutManager manager;
+    private ArrayList<NavigationBean.DataBean> dataBeans;
+    private MyNavigationAdapter adapter;
 
     public NavigationFragment() {
         // Required empty public constructor
@@ -55,7 +64,7 @@ public class NavigationFragment extends BaseFragment<NavigationV, NavigationP> i
         unbinder = ButterKnife.bind(this, inflate);
         return inflate;
     }*/
-
+//
     @Override
     protected NavigationP initPresenter() {
         return new NavigationP();
@@ -68,14 +77,53 @@ public class NavigationFragment extends BaseFragment<NavigationV, NavigationP> i
 
     @Override
     protected void initView() {
+        dataBeans = new ArrayList<>();
         manager = new LinearLayoutManager(getActivity());
         mLv.setLayoutManager(manager);
+        adapter = new MyNavigationAdapter(getActivity(), dataBeans);
+        mLv.setAdapter(adapter);
+
+        adapter.setOnClickListener(new MyNavigationAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position, int newPosition) {
+                Intent intent = new Intent(getActivity(),FlowWebActivity.class);
+                intent.putExtra("web",dataBeans.get(position).getArticles().get(newPosition).getLink());
+                getActivity().startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void initData() {
         mPresenter.getNavigation();
     }
+
+    @Override
+    protected void initListenter() {
+            //RecyclerView和tab栏联动
+        mLv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                mTab.setTabSelected(manager.findFirstVisibleItemPosition());
+            }
+        });
+        //tab栏和RecyclerView联动
+        mTab.addOnTabSelectedListener(new VerticalTabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabView tab, int position) {
+                manager.scrollToPositionWithOffset(position,0);
+            }
+
+            @Override
+            public void onTabReselected(TabView tab, int position) {
+
+            }
+        });
+
+
+    }
+
 
     @Override
     public void getData(NavigationBean bean) {
@@ -110,26 +158,8 @@ public class NavigationFragment extends BaseFragment<NavigationV, NavigationP> i
                 return 0;
             }
         });
-     /*   //RecyclerView和tab栏联动
-        mLv.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                mTab.setTabSelected(manager.findFirstVisibleItemPosition());
-            }
-        });
-        //tab栏和RecyclerView联动
-        mTab.addOnTabSelectedListener(new VerticalTabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabView tab, int position) {
-                manager.scrollToPositionWithOffset(position,0);
-            }
-
-            @Override
-            public void onTabReselected(TabView tab, int position) {
-
-            }
-        });*/
+        dataBeans.addAll(bean.getData());
+        adapter.notifyDataSetChanged();
     }
 
 }
