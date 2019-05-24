30f1d0d81f9a2833d3b7db6ffe4ec982447dc0db
wangbeibeibei <1714541556@qq.com>
2019-05-24 10:49:54 +0800

    解决冲突

--- app/src/main/java/com/jiyun/wanandroid/MaxActivity.java
+++ app/src/main/java/com/jiyun/wanandroid/MaxActivity.java
@@ -1,21 +1,14 @@
 package com.jiyun.wanandroid;
 
-import android.animation.ObjectAnimator;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.os.CountDownTimer;
+
 import android.os.Handler;
-<<<<<<< HEAD
-import android.os.Message;
-=======
->>>>>>> e9872ac8263e2e55812e4b571aee5d264e78a347
+
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.jiyun.wanandroid.ui.MainActivity;
@@ -54,21 +47,6 @@ public class MaxActivity extends AppCompatActivity{
     private Handler handler = new Handler();
     int time=1;
 
-    int i = 2;
-    Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            if (i > 0) {
-                i--;
-                handler.sendEmptyMessageDelayed(1000, 1000);
-            } else {
-                Intent intent = new Intent(MaxActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        }
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,30 +59,9 @@ public class MaxActivity extends AppCompatActivity{
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
-<<<<<<< HEAD
-    private void firstRun() {
-        SharedPreferences sharedPreferences = getSharedPreferences("FirstRun", 0);
-        Boolean first_run = sharedPreferences.getBoolean("First", true);
-        if (first_run) {
-            sharedPreferences.edit().putBoolean("First", false).commit();
-            //图片缩放
-            ObjectAnimator alpha = ObjectAnimator.ofFloat(mMaxImg, "alpha", 1, 0, 1);
-            alpha.setDuration(3000);
-            alpha.start();
-        } else {
-            Intent intent = new Intent(MaxActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
 
-    private void initView() {
-        mMaxImg = (ImageView) findViewById(R.id.max_img);
-        mTime = (TextView) findViewById(R.id.time);
 
-        firstRun();
-        handler.sendEmptyMessageDelayed(1000, 1000);
-=======
+
     private void inSkip() {
         handler.postDelayed(new Runnable() {
             @Override
@@ -153,6 +110,5 @@ public class MaxActivity extends AppCompatActivity{
         if (mLottieAnimationView != null) {
             mLottieAnimationView.cancelAnimation();
         }
->>>>>>> e9872ac8263e2e55812e4b571aee5d264e78a347
     }
 }
